88dfb83a2907513ba233b40db2134d02fcd7e513
Babatunde Awotimilehin <73068722+Olatundeawo@users.noreply.github.com>
2022-09-09 18:53:59 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive/n " /n)
+		printf("%d is positive/n ", n)
 	}
 	if else(n < 0)
 	{
-		printf("is negative/n " /n)
+		printf("%d is negative/n ", n)
 	}
 	if else(n == 0)
 	{
-		printf("is zero/n " /n)
+		printf("%d is zero/n ", n)
 	}
 	return (0);
 }
