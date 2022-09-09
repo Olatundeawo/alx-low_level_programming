7ac71df98dc04889dda2a18721c77de3b5540fe1
Babatunde Awotimilehin <73068722+Olatundeawo@users.noreply.github.com>
2022-09-09 04:54:14 +0200

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main entry
+*
+* function
+*
+*return value
+*/
+
+int main(void)
+{
+      
+        puts( "Programming is like building a multilingual puzzle/n");
+        return(0)
+  }
