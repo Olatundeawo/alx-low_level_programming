6f88a0a61a8fa89be58e201e94b8d76d64816f8d
Babatunde Awotimilehin <73068722+Olatundeawo@users.noreply.github.com>
2022-09-14 08:40:35 +0200

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  * _abs - Computes the absolute value of an integer.
+ * @n: integer from which to get an absolute value
+ * Return: Void.
+ */
+
+int _abs(int n)
+{
+
+if (n < 0)
+
+{
+return (n * -1);
+}
+return (n);
+}
