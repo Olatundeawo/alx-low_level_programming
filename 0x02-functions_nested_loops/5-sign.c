41a5bc315052ffb70921b247814d4a2d22d4b45e
Babatunde Awotimilehin <73068722+Olatundeawo@users.noreply.github.com>
2022-09-14 23:02:17 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
   * print_sign - Prints the sign of a number
   * @n: The number to be checked
   *
-  * Description: function that prints the last digit of a number.
+  * Description: function that prints the sign of a number.
   * Return: 1 for positive num, -1 for negative num or zero for anything else
   */
 int print_sign(int n)
