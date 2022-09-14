f4a446023dff248f4b5ebdfa70ba57c5b1b413ad
Babatunde Awotimilehin <73068722+Olatundeawo@users.noreply.github.com>
2022-09-14 23:03:21 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,6 +5,7 @@
 * print_last_digit - Prints the last digit of a number
 * @n: The number to be treated
 *
+* Description: function that prints the last digit of a number. 
 * Return: Value of the last digit of number
 */
 int print_last_digit(int n)
