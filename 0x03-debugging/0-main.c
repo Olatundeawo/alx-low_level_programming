5f9abadc683aedf51978cc7198c2abb128fd9baa
Olatundeawo <awotimilehin.babatunde>
2022-09-15 09:26:52 -0700

    mod

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+	int main(void)
+{
+	int i;
+
+	i = o;
+
+	positive_or_negative(i);
+
+	return (0);	
+}
