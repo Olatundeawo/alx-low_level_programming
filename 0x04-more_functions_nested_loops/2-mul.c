1b8c8413d2f407c8abef354baaeb17b7286c2e15
Olatundeawo <awotimilehin.babatunde>
2022-09-14 23:40:31 -0700

    mod

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - mulitipy two integer.
+ * @a: The first Integer to be mulitipied.
+ * @b: The second integer to be multiplied.
+ *
+ * Return:the result.
+ */
+
+	int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
