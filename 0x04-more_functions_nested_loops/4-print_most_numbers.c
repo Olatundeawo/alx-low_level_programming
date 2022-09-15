3df7bcb2ed7d1d80a2fc324ac5429d5c82b5e3fa
Olatundeawo <awotimilehin.babatunde>
2022-09-15 07:38:44 -0700

    mod

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - print an integer from 0 to 9 but not 2 and 4.
+ *
+ * Return: Always 0.
+ */
+
+	void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 4 || 2)
+		{
+			continue;
+		}
+		_putchar(i);
+		_putchar('\n');
+	}
+}
