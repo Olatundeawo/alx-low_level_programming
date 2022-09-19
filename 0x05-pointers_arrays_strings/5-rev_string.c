fd09d6e09dcfafdc52152db8ce442cbe0249ea83
Babatunde Awotimilehin <73068722+Olatundeawo@users.noreply.github.com>
2022-09-19 16:17:38 +0200

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string, in reversed.
+ *
+ * @s: pointer argument.
+ */
+
+	void print_rev(char *s)
+{
+	int i,j, len = 0;
+	char rev[];
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	j = len - 1;
+
+	for (i = 0; i < len; i++;)
+	{
+	rev[i] = s[j];
+	j--;
+	}
+	_putchar(rev);
+	_putchar('\n');
+}
