3d2890f955fa8e6d59d2f93c3a7d635a0f9351d5
Olatundeawo <awotimilehin.babatunde>
2022-09-19 06:29:28 -0700

    mod

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string, in reversed.
+ *
+ * @s: pointer argument.
+ */
+
+	void print_rev(char *s)
+{
+	int count = 0, index;
+
+	while (s[index++])
+
+		count++;
+
+	for (index = count -1; index >= 0; index--)
+{
+	_putchar(s[index]);
+}
+	_putchar('\n');
+}
