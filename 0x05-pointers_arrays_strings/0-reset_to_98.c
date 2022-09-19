6332c052b5ebeb70257d23baa1aeb5bd873a229d
Olatundeawo <awotimilehin.babatunde>
2022-09-19 03:08:12 -0700

    added some files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function containing a pointer.
+ *
+ * @*n: is a pointer
+ */
+
+	void reset_to_98(int *n)
+{
+	n = 98;
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+this files is all about 
+usin:
+1. pointer.
+2. Arrays.
+3. Strings.
