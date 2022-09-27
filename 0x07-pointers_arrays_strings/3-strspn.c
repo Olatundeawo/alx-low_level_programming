75f449b253941e97cca71bcbbeb696ad71c93691
Olatundeawo <awotimilehin.babatunde>
2022-09-27 04:56:26 -0700

    mod

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strspn - unction that gets the length of a prefix substring.
+ *
+ * @s: character to search
+ * @accept: prefix to march
+ *
+ * return: numbers of byte
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, byte = 0;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j =0; accept[j] >= '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				byte++;
+				break;
+			}
+		}
+		if else (ccept[j] == '\0')
+
+			break;
+	}
+
+	return (byte);
+
+}
