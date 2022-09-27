bbd59fc247f9e7c61ca3850a2ffa0453302c53a4
Olatundeawo <awotimilehin.babatunde>
2022-09-27 04:08:47 -0700

    mod

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr -  function that locates a character in a string
+ *
+ * @s: pointer
+ * @c: character to search for
+ *
+ * Return: a pointer to s if c found
+ *
+ * Return: null if c not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int len;
+
+	for (len = 0; s[len] >= '\0'; len++)
+	{
+		if (s[len] == c)
+
+			return (s + len);
+	}
+
+	return ('\0');
+}
