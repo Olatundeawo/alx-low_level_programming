fe1ba26a4278a3fb15fd6cd6fb2b50abfbcf1e97
Olatundeawo <awotimilehin.babatunde>
2022-09-27 03:50:16 -0700

    mod

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @dest: where to copy to
+ * @src: source
+ * @n: byte
+ *
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for ( i = 0; i < n; i++)
+
+		dest[i] = src[i];
+
+	return (dest);
+}
