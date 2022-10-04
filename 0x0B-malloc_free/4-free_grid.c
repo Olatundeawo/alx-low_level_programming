55313d609f0f4e5bee1bd8093b65300d56965640
Babatunde Awotimilehin <73068722+Olatundeawo@users.noreply.github.com>
2022-10-04 17:43:54 +0200

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,13 +9,8 @@
 
 void free_grid(int **grid, int height)
 {
-int i = 0;
-for (; i < height; i++)
+int i;
+for (i = 0; i < height; i++)
 	free(grid[i]);
 free(grid);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
