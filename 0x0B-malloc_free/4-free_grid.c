8e8877c84d9dd5a30b012c2f74a01e84ebc00c26
Babatunde Awotimilehin <73068722+Olatundeawo@users.noreply.github.com>
2022-10-04 17:25:58 +0200

    Create 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - free the grid
+ * @grid: grid of memories
+ * @height: int
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+int i = 0;
+for (; i < height; i++)
+	free(grid[i]);
+free(grid);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
