b027452ba28fa18cb51c972000394ccc7773c10b
Olatundeawo <awotimilehin.babatunde>
2022-10-06 08:39:50 -0700

    mmod

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory
+ *
+ * @b: int
+ *
+ * Return: null
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *s
+
+		while (1)
+	{
+		s = malloc(INT_NULL);
+		if (s == NULL)
+			return (98);
+
+		return (0);
+	}
+}
