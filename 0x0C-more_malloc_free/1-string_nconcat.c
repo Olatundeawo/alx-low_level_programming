2d95818d2e1850a3c1a192bd74975f0241eb5603
Olatundeawo <awotimilehin.babatunde>
2022-10-06 09:51:48 -0700

    mod

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len = 0;
 
-	for (i = 0; s1[i]; i++;)
+	for (i = 0; s1[i]; i++)
 		s[len++] = s1[i];
 
 	for (i = 0; s2[i] && i < n; i++)
