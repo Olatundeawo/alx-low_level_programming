b9fbb7127187dab61c60dc059ac18c342b2a076d
Olatundeawo <awotimilehin.babatunde>
2022-10-10 13:21:54 -0700

    added new_dog.c file

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,89 @@
+#include "dog.h"
+
+int strlen(char *str);
+char *strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * strlen - find string length
+ *
+ * @str: string
+ *
+ * Return: len
+ */
+
+int strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+
+		len++;
+	return (len);
+}
+
+/**
+ * strcpy - copy string
+ *
+ * @dest: copy to
+ * @src: source
+ *
+ * Return: dest
+ */
+
+char *strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+
+/**
+ * new_dog - function to new dog
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: success or null if failed
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *mydog;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+	mydog = malloc(sizeof(dot_t)
+	if (mydog == NULL)
+		return (NULL);
+
+	mydog = malloc(sizeof(char) * (strlen(name) + 1));
+
+	if (mydog->name == NULL)
+	{
+		free(mydog);
+		return (NULL);
+	}
+
+	mydog = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (mydog->owner == NULL)
+	{
+		free(mydog->name);
+		free(mydog->owner);
+		return (NULL);
+	}
+
+	mydog->name = strcpy(mydog->name, name);
+	mydog->age = age;
+	mydog->owner = strcpy(mydog->owner, owner);
+
+	return (mydog);
+}
