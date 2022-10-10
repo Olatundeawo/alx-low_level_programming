66f40b6e63dc94138b79b09b6dba7ef8fabd5f66
Olatundeawo <awotimilehin.babatunde>
2022-10-10 10:34:09 -0700

    mod

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ *int _dog - dog
+ *
+ * @d: initialze variable
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog d;
+}
