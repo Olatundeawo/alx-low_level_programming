01bcdb648cf3a31b7f4a85bcba71b44cfb7b8e4e
Olatundeawo <awotimilehin.babatunde>
2022-10-20 07:57:24 -0700

    mod

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,11 +13,11 @@ size_t print_list(const list_t *h)
 	size_t elem = 0;
 
 	while (h != NULL)
-
+	{
 		if (h->str == NULL)
 			printf("[0] (nill)\n");
 		else
-	{
+	
 		printf("[%d] %s\n", h->data, h->str);
 
 		elem++;
