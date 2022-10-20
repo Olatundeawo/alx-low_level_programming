c283c207007b133d92a99d42e180f77991d0bc96
Babatunde Awotimilehin <73068722+Olatundeawo@users.noreply.github.com>
2022-10-20 18:18:47 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,20 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t elem = 0;
+	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nill)\n");
+			printf("[0] (nil)\n");
+
 		else
+			printf("[%d] %s\n", h->len, h->str);
 
-		printf("[%i] %s\n", h->len, h->str);
-
-
-		elem++;
+		nodes++;
 		h = h->next;
 	}
 
-	return (elem);
-}
+	return (nodes);
+}}
